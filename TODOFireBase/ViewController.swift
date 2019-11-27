10b33cb0a2e8190e488741568fa2d64f36a395bb
dipalpatel913@gmail.com <dipalpatel913@gmail.com>
2019-11-27 13:38:12 -0500

    Initial Commit

---
+++ TODOFireBase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TODOFireBase
+//
+//  Created by Dipal Patel on 2019-11-27.
+//  Copyright © 2019 Dipal Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
